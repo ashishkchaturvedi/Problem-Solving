365eef7d5b3320a6aae22d5f7b7dd4398d0c2a9f
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2019-11-24 08:15:12 -0500

    Adding DFS Graph

--- src/com/graph/DFS.java
+++ src/com/graph/DFS.java
@@ -1,9 +1,67 @@
 package com.graph;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
+
 public class DFS {
+	
+	private static int V;
+	private static LinkedList<Integer> adjList[];
+	
+	@SuppressWarnings("unchecked")
+	DFS(int v){
+		DFS.V = v;
+		adjList = new LinkedList[v];
+		for(int i = 0; i < v; i++){
+			adjList[i] = new LinkedList<>(); 
+		}
+	}
+	
+	void addEdge(DFS g,int src,int dest){
+		DFS.adjList[src].add(dest);
+	}
+	
+	static void DFSUtil(int v,boolean visited[]) 
+    { 
+        // Mark the current node as visited and print it 
+        visited[v] = true; 
+        System.out.print(v+" "); 
+  
+        // Recur for all the vertices adjacent to this vertex 
+        Iterator<Integer> i = adjList[v].listIterator(); 
+        while (i.hasNext()) 
+        { 
+            int n = i.next(); 
+            if (!visited[n]) 
+                DFSUtil(n, visited); 
+        } 
+    } 
+  
+    // The function to do DFS traversal. It uses recursive DFSUtil() 
+    static void traverse(int v) 
+    { 
+        // Mark all the vertices as not visited(set as 
+        // false by default in java) 
+        boolean visited[] = new boolean[V]; 
+  
+        // Call the recursive helper function to print DFS traversal 
+        DFSUtil(v, visited); 
+    } 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		
+		DFS g = new DFS(4);
+		
+		g.addEdge(g, 0, 1);
+		g.addEdge(g, 0, 2);
+		g.addEdge(g, 1, 2);
+		g.addEdge(g, 2, 0);
+		g.addEdge(g, 2, 3);
+		g.addEdge(g, 3, 3);
+		
+		traverse(0);
 
 	}
 
