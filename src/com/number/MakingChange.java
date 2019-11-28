6f0ba9663aa74aac00394ddf32e23e7532b0c742
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2019-11-28 09:48:22 -0500

    modifying making change problem

--- src/com/number/MakingChange.java
+++ src/com/number/MakingChange.java
@@ -8,12 +8,28 @@ public class MakingChange {
 	}
 	
 	public static int change(int x, int[] coins){
+		
+		int[] cache = new int[x];
+		for(int i = 1; i < x; i++){
+			cache[i] = -1;
+		}
+		return change(x, coins, cache);
+	}
+	
+	public static int change(int x, int[] coins, int[] cache){
 		if (x == 0) return 0;
 		
 		int min = x;
 		for(int coin : coins){
 			if(x- coin >= 0){
-				int c = change(x - coin, coins);
+				int c;
+				if(cache[x-coin] >= 0){
+					c = cache[x - coin];
+				}
+				else{
+					c = change(x-coin, coins, cache);
+					cache[x - coin] = c;
+				}
 				if(min > c + 1) {
 					min = c + 1;
 				}
