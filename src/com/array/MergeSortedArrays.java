1ad09d368f5eb024daf6efcc6136fd7648a82268
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2019-12-10 21:52:28 -0500

    Zig Zag Tree traversal

--- src/com/array/MergeSortedArrays.java
+++ src/com/array/MergeSortedArrays.java
@@ -13,14 +13,15 @@ public class MergeSortedArrays {
 	}
 	
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int l = nums1.length - 1;
+        
+    	
+    	int l = nums1.length - 1;
         
         while(m > 0 && n > 0){
         	if(nums1[m-1] < nums2[n-1]){
 
         		System.out.println("n is:" +n);
         		nums1[l] = nums2[n-1];
-        		//System.out.println(nums1[l]);
         		l--;
         		n--;
         	}
@@ -28,7 +29,6 @@ public class MergeSortedArrays {
 
         		System.out.println("m is: "+m);
         		nums1[l] = nums1[m-1];
-        		//System.out.println(nums1[l]);
         		l--;
         		m--;
         	}
---
+++ src/com/tree/ZigZagTraversal.java
@@ -0,0 +1,84 @@
+package com.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+//class TreeNode{
+//	int val;
+//	TreeNode left;
+//	TreeNode right;
+//	
+//	public TreeNode(int k){
+//		this.key = k;
+//		this.left = null;
+//		this.right = null;
+//	}
+//}
+
+public class ZigZagTraversal {
+
+	
+	public static void main(String[] args) {
+		
+		TreeNode root = new TreeNode(3);
+		root.left = new TreeNode(9);
+		root.right = new TreeNode(20);
+		root.right.left = new TreeNode(15);
+		root.right.right = new TreeNode(7);
+		
+		List<List<Integer>> res = new ArrayList();
+		
+		res = zigzagLevelOrder(root);
+		
+		System.out.println("[");
+		for(List<Integer> l : res){
+			System.out.print("[");
+			for(int i : l){
+				System.out.print(i + " ");
+			}
+			System.out.print("]");
+			System.out.println();
+		}
+		System.out.println("]");
+	}
+		
+	
+	public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+		
+      List<List<Integer>> res = new ArrayList<List<Integer>>();
+      if(root == null) return res;
+      
+      Queue<TreeNode> q = new LinkedList<TreeNode>();
+      q.offer(root);
+      boolean isOdd = true;
+      
+      while(!q.isEmpty()){
+    	  List<Integer> level = new ArrayList<>();
+    	  int qSize = q.size();
+    	  
+    	  for(int i = 0; i < qSize; i++){
+    		  root = q.poll();
+    		  if(isOdd){
+    			  level.add(root.key);
+    		  }
+    		  else{
+    			level.add(0, root.key);  
+    		  }
+    		  if(root.left != null) q.offer(root.left);
+    		  if(root.right != null) q.offer(root.right);
+    	  }
+    	  
+    	  if(isOdd){
+    		  isOdd = false;
+    	  }
+    	  else{
+    		  isOdd = true;
+    	  }
+    	  res.add(level);
+      }
+      return res;
+    }
+
+}
