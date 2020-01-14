2882015fea065b5e1831c1bc82954fe392f41ecc
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2020-01-14 08:13:03 -0500

    Add digits

---
+++ src/com/number/AddDigit.java
@@ -0,0 +1,20 @@
+package com.number;
+
+public class AddDigit {
+
+	public static void main(String[] args) {
+		
+		int num = 49;
+		System.out.println(addDigits(num));
+
+	}
+	
+	public static int addDigits(int num) {
+		if(num == 0) return 0;
+		
+		if(num % 9 == 0) return 9;
+		
+		else return num % 9;
+	}
+
+}
