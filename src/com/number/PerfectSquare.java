632342887a7a9cde3862fe2d617f43b334801944
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2020-01-20 09:24:48 -0500

    Perfect Squares

---
+++ src/com/number/PerfectSquare.java
@@ -0,0 +1,29 @@
+package com.number;
+
+import java.util.Arrays;
+
+public class PerfectSquare {
+
+	public static void main(String[] args) {
+		int n = 13;
+		System.out.println(numSquares(n));
+
+	}
+	
+    public static int numSquares(int n) {
+    	int[] dp = new int[n + 1];
+    	Arrays.fill(dp, Integer.MAX_VALUE);
+    	dp[0] = 0;
+    	for(int i = 1; i <= n; ++i) {
+    		int min = Integer.MAX_VALUE;
+    		int j = 1;
+    		while(i - j*j >= 0) {
+    			min = Math.min(min, dp[i - j*j] + 1);
+    			++j;
+    		}
+    		dp[i] = min;
+    	}		
+    	return dp[n];
+    }
+
+}
