bf1b25c8967539a73dd884ba79bf837aed11e929
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2020-01-27 07:42:59 -0500

    Swap nodes in pairs

---
+++ src/com/linkedList/SwapPairsInNode.java
@@ -0,0 +1,28 @@
+package com.linkedList;
+
+public class SwapPairsInNode {
+
+	public static void main(String[] args) {
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		
+		ListNode res = swapPairs(head);
+		while(res != null){
+			System.out.println(res.val);
+			res = res.next;
+		}
+
+	}
+	
+    public static ListNode swapPairs(ListNode head) {
+        if ((head == null)||(head.next == null))
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+
+}
