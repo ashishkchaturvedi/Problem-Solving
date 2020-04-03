0382437fb9e7092fc72e7803161ab880e39b4abf
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2020-04-02 22:01:04 -0400

    First and last position of element in sorted array

--- src/com/array/FirstLastPositionSortedArray.java
+++ src/com/array/FirstLastPositionSortedArray.java
@@ -12,23 +12,38 @@ public class FirstLastPositionSortedArray {
 	}
 	
 	public static int[] searchRange(int[] nums, int target) {
-        int start = 0;
+		int start = 0;
         int end = nums.length - 1;
-            
+        int l = -1;
+        int r = -1;
         if(end == -1) return new int[]{-1,-1};
         if(nums[0] == target && nums[end] == target) return new int[]{0,end};
         
         if(nums[0] == target){
+            l = r = 0;
             if(nums.length > 1){
-                if(nums[1] == target) return new int[]{0,1};
-                else return new int[]{0,0};
+                if(nums[1] == target){
+                    r = 1;
+                    while(nums[r] == target){
+                        r++;
+                    }
+                    r--;
+                }
+                return new int[]{l,r};
             }
-            else return new int[]{0,0};
+            else return new int[]{l,r};
         }
         
         if(nums[end] == target){
-            if(nums[end-1] == target) return new int[]{end-1,end};
-            else return new int[]{end,end};
+            l = r = end;
+            if(nums[end-1] == target) {
+                l = end-1;
+                while(nums[l] == target){
+                    l--;
+                }
+                l++;
+            }
+            return new int[]{l,r};
         }
         
         System.out.println("I am here");
@@ -36,14 +51,22 @@ public class FirstLastPositionSortedArray {
             int mid = start + (end - start) / 2;
             System.out.println("mid is:"+mid);
             if(nums[mid] == target){
-                System.out.println("I am here, mid is:" +mid);
+                l = r = mid;
                 if(nums[mid-1] == target){
-                    return new int[] {mid-1, mid};
+                    l = mid-1;
+                    while(nums[l] == target){
+                        l--;
+                    }
+                    l++;
                 }
-                else if(nums[mid+1] == target){
-                    return new int[] {mid, mid+1};
+                if(nums[mid+1] == target){
+                    r = mid+1;
+                    while(nums[r] == target){
+                        r++;
+                    }
+                    r--;
                 }
-                else return new int[]{mid,mid};
+                return new int[]{l,r};
             }
             else if(nums[mid] < target) {
                 start = mid + 1;
@@ -52,7 +75,7 @@ public class FirstLastPositionSortedArray {
                 end = mid - 1;
             }
         }
-        return new int[]{-1,-1};
+        return new int[]{-1,-1}; 
     }
 
 }
