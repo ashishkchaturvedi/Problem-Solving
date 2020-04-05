5e5dbf898cf25c09b275ede7a10309e001c7ef25
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2020-04-05 16:50:09 -0400

    Next permutation

---
+++ src/com/array/NextPermutation2.java
@@ -0,0 +1,51 @@
+package com.array;
+
+import java.util.Arrays;
+
+public class NextPermutation2 {
+
+	public static void main(String[] args) {
+		
+		int nums[] = {0, 1, 2, 5, 3, 3, 0};
+		nextPermutation(nums);
+	}
+	
+	public static void nextPermutation(int[] nums){
+		
+		int i = nums.length - 1;
+		
+		while(i > 0 && nums[i - 1] >= nums[i]){
+			i--;
+		}
+		
+		if(i <= 0){
+			Arrays.sort(nums);
+		}
+		else{
+			int j = nums.length - 1;
+			while(nums[i-1] >= nums[j]){
+				j--;
+			}
+			nums[i-1] = nums[i-1] + nums[j];
+			nums[j] = nums[i-1] - nums[j];
+			nums[i-1] = nums[i-1] - nums[j];
+			
+			j = nums.length - 1;
+			
+			while(i < j){
+				nums[i] = nums[i] + nums[j];;
+				nums[j] = nums[i] - nums[j];
+				nums[i] = nums[i] - nums[j];
+				i++;
+				j--;
+			}
+		}
+		
+		for(int k : nums){
+			System.out.print(k+",");
+		}
+		
+
+	}
+
+}
