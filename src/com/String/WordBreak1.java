9f816a8edca484b8b8178235f5cb6252d71401e9
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2020-04-11 17:26:30 -0400

    Word Breas

---
+++ src/com/String/WordBreak1.java
@@ -0,0 +1,36 @@
+package com.String;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordBreak1 {
+
+	public static void main(String[] args) {
+		
+		String s = "leetcode";
+		List<String> wordDict = new ArrayList<String>();
+		wordDict.add("leet");
+		wordDict.add("code");
+		System.out.println(wordBreak(s, wordDict));
+
+	}
+	
+	public static boolean wordBreak(String s, List<String> wordDict){
+		return word_Break(s, new HashSet<String>(wordDict), 0, new Boolean[s.length()]);
+	}
+
+	private static boolean word_Break(String s, Set<String> wordDict, int start, Boolean[] memo) {
+		if(start == s.length()) return true;
+		if(memo[start] != null) return memo[start];
+		
+		for(int end = start + 1; end <= s.length(); end++){
+			if(wordDict.contains(s.substring(start, end)) && word_Break(s, wordDict, end, memo)){
+				return memo[start] = true;
+			}
+		}
+		return memo[start] = false;
+	}
+
+}
