8062a3566a4cfc548e8c847027d4415c028c8696
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2020-04-24 14:10:26 -0400

    LRU Cache

--- src/com/number/LRUCache3.java
+++ src/com/number/LRUCache3.java
@@ -1,9 +1,50 @@
 package com.number;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class LRUCache3 {
+	
+	Map<Integer, Integer> cache;
+	int capacity;
+	
+	public LRUCache3(int capacity){
+		this.capacity = capacity;
+		this.cache = new LinkedHashMap<Integer, Integer>();
+	}
+	
+	public int get(int key){
+		if(cache.containsKey(key)){
+			int v = cache.get(key);
+			cache.remove(key);
+			cache.put(key, v);
+			return v;
+		}
+		return -1;
+	}
+	
+	public void put(int key, int value){
+		if(cache.containsKey(key)){
+			cache.remove(key);
+		}
+		else if(cache.size() == capacity){
+			cache.remove(cache.entrySet().iterator().next().getKey());
+		}
+		cache.put(key, value);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		LRUCache3 lru = new LRUCache3(2);
+		lru.put(1, 1);
+		lru.put(2, 2);
+		System.out.println(lru.get(1));
+		lru.put(3, 3);
+		System.out.println(lru.get(2));
+		lru.put(4, 4);
+		System.out.println(lru.get(1));
+		System.out.println(lru.get(3));
+		System.out.println(lru.get(4));
 
 	}
 
